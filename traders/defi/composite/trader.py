c7bbd927476d3c081787b6856bad1fa6b3257428
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-06 18:10:48 +0500

    Added SturdyTrader

--- traders/defi/composite/trader.py
+++ traders/defi/composite/trader.py
@@ -7,6 +7,7 @@ from head.decorators.yieldmethod import yieldmethod
 from traders.defi.components.curve.trader import CurveTrader
 from traders.defi.components.ellipsis.trader import EllipsisTrader
 from traders.defi.components.convex.trader import ConvexTrader
+from traders.defi.components.sturdy.trader import SturdyTrader
 
 
 @singleton
@@ -29,3 +30,4 @@ defiTrader = DeFiTrader()
 defiTrader.addTrader(trader=CurveTrader)
 defiTrader.addTrader(trader=EllipsisTrader)
 defiTrader.addTrader(trader=ConvexTrader)
+defiTrader.addTrader(trader=SturdyTrader)
