63fe6c71799eec20cce1b6ca068efcdbed14deef
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-16 01:58:59 +0500

    Removed FTX trader

--- traders/cefi/composite/trader.py
+++ traders/cefi/composite/trader.py
@@ -7,7 +7,6 @@ from head.decorators.yieldmethod import yieldmethod
 from traders.cefi.components.binance.trader import BinanceTrader
 from traders.cefi.components.gateio.trader import GateioTrader
 from traders.cefi.components.coingecko.trader import CoingeckoTrader
-from traders.cefi.components.ftx.trader import FTXTrader
 
 
 @singleton
@@ -27,7 +26,6 @@ class CeFiTrader(ITraderComponent):
 
 cefiTrader = CeFiTrader()
 
-cefiTrader.addTrader(trader=FTXTrader)
 cefiTrader.addTrader(trader=BinanceTrader)
 cefiTrader.addTrader(trader=GateioTrader)
 cefiTrader.addTrader(trader=CoingeckoTrader)
